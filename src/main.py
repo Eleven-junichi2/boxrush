abfd786eb06ac4f3ab9f9d1ea25a09892f953c01
Eleven-junichi2 <junichinomori@yahoo.co.jp>
2020-09-21 19:03:18 +0900

    update function

--- src/main.py
+++ src/main.py
@@ -1,9 +1,12 @@
-# import pathlib
+import pathlib
 import sys
 from typing import Tuple
 
 import pygame
 
+MAIN_PRG_DIR = pathlib.Path(__file__).absolute().parent
+ASSET_DIR = MAIN_PRG_DIR / "assets"
+FONT_DIR = ASSET_DIR / "fonts"
 SCRN_WIDTH = 512
 SCRN_HEIGHT = 434
 SCRN_SIZE = SCRN_WIDTH, SCRN_HEIGHT
@@ -11,33 +14,38 @@ BLACK = 0, 0, 0
 WHITE = 255, 255, 255
 
 
-def text_coordinate_to_center(screen_size: Tuple, text_size: Tuple) -> Tuple:
+def text_pos_to_center(screen_size: Tuple, text_size: Tuple,
+                       multiply_to_fix_pos_x=1, multiply_to_fix_pos_y=1, ):
     """find coordinate to center text
     screen_size: (width, height)
     text_size: (width, height)
     """
-    coodinate = (screen_size[0]*0.5-text_size[0]*0.5,
-                 screen_size[1]*0.5-text_size[1]*0.5)
-    return coodinate
+    return ((screen_size[0]*0.5-text_size[0]*0.5)*multiply_to_fix_pos_x,
+            (screen_size[1]*0.5-text_size[1]*0.5)*multiply_to_fix_pos_y)
 
 
 def main():
     pygame.init()
     screen = pygame.display.set_mode(SCRN_SIZE)
     pygame.display.set_caption("Boxrush")
-    font = pygame.font.Font("./assets/fonts/misaki_gothic_2nd.ttf", 32)
-    title_text = font.render("美咲フォントおすすめ", True, WHITE)
+    font = pygame.font.Font(str(FONT_DIR / "misaki_gothic_2nd.ttf"), 32)
+    title_text = font.render("Boxrush", True, WHITE)
+    title_pos = (
+        text_pos_to_center(
+            screen.get_size(), title_text.get_size(), 1, 0.25))
     while 1:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 sys.exit()
         screen.fill(BLACK)
         # --draw here--
-        screen.blit(title_text, text_coordinate_to_center(
-            screen.get_size(), title_text.get_size()))
+        screen.blit(title_text, title_pos)
         # ----
         pygame.display.update()
 
 
 if __name__ == "__main__":
-    main()
+    try:
+        main()
+    except SystemExit:
+        pass
