cbac02d419e1ea16afd9a62a81f8fea615b9056e
Eleven-junichi2 <junichinomori@yahoo.co.jp>
2020-09-21 15:34:16 +0900

    rename variables to more readable

--- src/main.py
+++ src/main.py
@@ -4,14 +4,16 @@ import pygame
 
 def main():
     pygame.init()
-    size = width, height = 512, 434
-    black = 0, 0, 0
-    screen = pygame.display.set_mode(size)
+    SCRN_WIDTH = 512
+    SCRN_HEIGHT = 434
+    SCRN_SIZE = SCRN_WIDTH, SCRN_HEIGHT
+    BLACK = 0, 0, 0
+    screen = pygame.display.set_mode(SCRN_SIZE)
     while 1:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 sys.exit()
-        screen.fill(black)
+        screen.fill(BLACK)
         # --draw here--
 
         # ----
