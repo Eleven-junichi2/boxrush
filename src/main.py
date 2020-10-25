fda05d59e8ff1fb4245500104767911ae819a8f1
Eleven-junichi2 <junichinomori@yahoo.co.jp>
2020-10-25 17:40:11 +0900

    add function: edit map

--- src/main.py
+++ src/main.py
@@ -4,6 +4,7 @@ import sys
 
 import pygame
 
+GAME_TITLE = "YUMA"
 MAIN_PRG_DIR = pathlib.Path(__file__).absolute().parent
 SCRN_WIDTH = 512
 SCRN_HEIGHT = 434
@@ -14,9 +15,23 @@ KEY_REPEAT_DELAY = 125
 KEY_REPEAT_INTERVAL = 125
 
 
+class SceneManager:
+    def __init__(self, screen: pygame.Surface, asset_path):
+        self.screen = screen
+        self.asset_path = asset_path
+        self.scene_list = {}
+        self.current_scene = None
+
+    def append_scene(self, scene_name, scene):
+        self.scene_list[scene_name] = scene
+
+    def set_current_scene(self, scene_name):
+        self.current_scene = self.scene_list[scene_name]
+
+
 class Scene:
-    def __init__(self, screen_manager):
-        self.sm = screen_manager
+    def __init__(self, scene_manager: SceneManager):
+        self.sm = scene_manager
 
     def handle_event(self, event):
         pass
@@ -27,20 +42,6 @@ class Scene:
     def render(self):
         self.sm.screen.fill(BLACK)
         pygame.display.update()
-
-
-class SceneManager:
-    def __init__(self, screen, asset_path):
-        self.screen = screen
-        self.asset_path = asset_path
-        self.scene_list = {}
-        self.current_scene = None
-
-    def append_scene(self, scene_name, scene: Scene):
-        self.scene_list[scene_name] = scene
-
-    def set_current_scene(self, scene_name):
-        self.current_scene = self.scene_list[scene_name]
 
 
 class TitleScene(Scene):
@@ -75,7 +76,7 @@ class TitleScene(Scene):
         self.sm.screen.fill(BLACK)
         font = pygame.font.Font(
             str(self.sm.asset_path.font_path("misaki_gothic_2nd.ttf")), 48)
-        title_text = font.render("Boxrush", True, WHITE)
+        title_text = font.render(GAME_TITLE, True, WHITE)
         font = pygame.font.Font(
             str(self.sm.asset_path.font_path("misaki_gothic_2nd.ttf")), 32)
         title_pos = (
@@ -96,6 +97,29 @@ class TitleScene(Scene):
 class GameScene(Scene):
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
+        self.prepared_terrain_map = False
+        self.terrain = Terrain()
+        MAP_HEIGHT = 16
+        MAP_WIDTH = 16
+        self.terrain.reset_map(3, MAP_HEIGHT, MAP_WIDTH)
+        self.terrain.fill_map(2, 3, 1, 4, 6, "Glass")
+
+    def render(self):
+        self.sm.screen.fill(BLACK)
+        self.render_terrain(self.terrain.map)
+        pygame.display.update()
+
+    def render_terrain(self, terrain_map):
+        sprite = SpriteSheet(self.sm.asset_path.img_path("skyeyebg.png"))
+        for z in range(len(terrain_map)):
+            for y in range(len(terrain_map[0])):
+                for x in range(len(terrain_map[0][0])):
+                    if terrain_map[z][y][x] == 1:
+                        self.sm.screen.blit(
+                            sprite.get_image(0, 0, 16, 16), (16*x, 16*y))
+                    elif terrain_map[z][y][x] == 2:
+                        self.sm.screen.blit(
+                            sprite.get_image(16, 0, 16, 16), (16*x, 16*y))
 
 
 class AssetPathGetter:
@@ -112,12 +136,12 @@ class AssetPathGetter:
 
 
 class SpriteSheet:
-    def __init__(self, filename) -> None:
-        self.sheet = pygame.image.load(filename).convert_alpha()
+    def __init__(self, filename):
+        self.sheet = pygame.image.load(str(filename)).convert_alpha()
 
     def get_image(self, x, y, width, height) -> pygame.Surface:
         image = pygame.Surface((width, height))
-        image.blit(self.spritesheet, (0, 0), (x, y, width, height))
+        image.blit(self.sheet, (0, 0), (x, y, width, height))
         return image
 
 
@@ -131,11 +155,66 @@ class PlayerSprite(pygame.sprite.Sprite):
         pass
 
 
+class Tile:
+    def __init__(self):
+        self.layer = 0
+        self.barrier = False
+        self.name = ""
+
+
+class WaterTile(Tile):
+    def __init__(self):
+        super().__init__()
+        self.name = "Water"
+        self.layer = 0
+
+
+class DirtTile(Tile):
+    def __init__(self):
+        super().__init__()
+        self.name = "Dirt"
+        self.layer = 1
+
+
+class GlassTile(Tile):
+    def __init__(self):
+        super().__init__()
+        self.name = "Dirt"
+        self.layer = 2
+
+
+def get_swap_dict(dictionary):
+    return {value: key for key, value in dictionary.items()}
+
+
+class Terrain:
+    def __init__(self):
+        self.tile_img_assign = {"Glass": "", "Dirt": "", "Water": ""}
+        self.tile_id_assign = {None: 0, "Glass": 1, "Dirt": 2, "Water": 3}
+        self.tile_type_from_id = get_swap_dict(self.tile_id_assign)
+        self.map = [[[]]]
+
+    def reset_map(self, layer_num, height, width, init_tile=None):
+        self.map = [[[self.tile_id_assign[init_tile] for x in range(
+            width)] for y in range(height)] for z in range(layer_num)]
+
+    def rewrite_map_tile(self, layer_id, x, y, tile):
+        self.map[layer_id][y][x] = self.tile_id_assign[tile]
+
+    def fill_map(self, layer_id, start_x, width, start_y, height, tile):
+        for y in range(start_y, start_y + height):
+            self.map[
+                layer_id][
+                    y][
+                start_x: start_x + width] = [
+                    self.tile_id_assign[tile] for x in range(width)]
+
+
 class Game:
-    def __init__(self, title="Boxrush"):
+    def __init__(self):
         pygame.init()
         pygame.key.set_repeat(KEY_REPEAT_DELAY, KEY_REPEAT_INTERVAL)
-        pygame.display.set_caption(title)
+        pygame.display.set_caption(GAME_TITLE)
         self.screen = pygame.display.set_mode(SCRN_SIZE)
         self.asset_path = AssetPathGetter(
             MAIN_PRG_DIR / "assets", "fonts", "imgs")
